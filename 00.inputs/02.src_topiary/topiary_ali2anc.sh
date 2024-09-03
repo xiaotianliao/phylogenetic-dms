7690dafc90a2f65be5361236b08d9b5f7e8f2414
Xiaotian Liao <xl7@sanger.ac.uk>
2024-09-03 13:55:05 +0100

    check dlg4 alignment

---
+++ 00.inputs/02.src_topiary/topiary_ali2anc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -G team354
+#BSUB -n 28  # Requesting 28 threads (cores)
+#BSUB -M 300000  # Memory requirement (adjust as needed)
+#BSUB -R "select[mem>300000] rusage[mem=300000] span[ptile=28]" 
+#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090324_%J.out
+#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090324_%J.err
+#BSUB -q week
+
+
+conda activate  /software/hgi/envs/conda/team354/xl7/topiary
+
+cd /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary
+
+topiary-alignment-to-ancestors /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/seed_to_ali/final_dataframe.csv --out_dir /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali_to_anc --num_threads 28
---
+++ 00.inputs/03.dlg4_topiary/topiary_seed2ali_dlg4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#BSUB -G team354
+#BSUB -n 10
+#BSUB -M 100000
+#BSUB -R "select[mem>100000] rusage[mem=100000]"
+#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/topiary_dlg4_090324_%J.out
+#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/topiary_dlg4_090324_%J.err
+#BSUB -q yesterday
+
+conda activate  /software/hgi/envs/conda/team354/xl7/topiary
+
+cd /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary
+
+#topiary-seed-to-alignment /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed-dataframe_dlg4.csv --out_dir  /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed_to_ali  --local_recip_blast_db /lustre/scratch126/gengen/projects/alpha-allostery-global/phylo-dms/01.blastdb/dlg4_localblast/combined_protein.faa
+topiary-seed-to-alignment /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed-dataframe_dlg4.csv --out_dir  /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed_to_ali  --local_recip_blast_db /lustre/scratch126/gengen/projects/alpha-allostery-global/phylo-dms/01.blastdb/dlg4_localblast_homo_danio/combined_protein.faa  
