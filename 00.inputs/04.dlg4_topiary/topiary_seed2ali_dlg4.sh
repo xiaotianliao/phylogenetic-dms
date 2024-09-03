e96ab850e8998cecad167ea25c29e2ed7adca897
Xiaotian Liao <xl7@sanger.ac.uk>
2024-09-03 16:25:03 +0100

    fix dlg4 realign

--- 00.inputs/04.dlg4_topiary/topiary_seed2ali_dlg4.sh
+++ 00.inputs/04.dlg4_topiary/topiary_seed2ali_dlg4.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #BSUB -G team354
 #BSUB -n 10
-#BSUB -M 100000
-#BSUB -R "select[mem>100000] rusage[mem=100000]"
-#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/topiary_dlg4_090324_%J.out
-#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/topiary_dlg4_090324_%J.err
+#BSUB -M 200000
+#BSUB -R "select[mem>200000] rusage[mem=200000]"
+#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/04.dlg4_topiary/topiary_dlg4_090324_%J.out
+#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/04.dlg4_topiary/topiary_dlg4_090324_%J.err
 #BSUB -q yesterday
 
 conda activate  /software/hgi/envs/conda/team354/xl7/topiary
 
-cd /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary
+cd /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/04.dlg4_topiary
 
-#topiary-seed-to-alignment /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed-dataframe_dlg4.csv --out_dir  /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed_to_ali  --local_recip_blast_db /lustre/scratch126/gengen/projects/alpha-allostery-global/phylo-dms/01.blastdb/dlg4_localblast/combined_protein.faa
-topiary-seed-to-alignment /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed-dataframe_dlg4.csv --out_dir  /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/03.dlg4_topiary/seed_to_ali  --local_recip_blast_db /lustre/scratch126/gengen/projects/alpha-allostery-global/phylo-dms/01.blastdb/dlg4_localblast_homo_danio/combined_protein.faa  
+topiary-seed-to-alignment /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/04.dlg4_topiary/seed-dataframe_dlg4.csv --out_dir  /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/04.dlg4_topiary/seed_to_ali  --local_recip_blast_db /lustre/scratch126/gengen/projects/alpha-allostery-global/phylo-dms/01.blastdb/dlg4_localblast_homo_danio/combined_protein.faa  
---
+++ 00.inputs/05.src_topiary_iqtree/src_topiary_iqtree_mf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -G team354
+#BSUB -n 24
+#BSUB -M 300000
+#BSUB -R "select[mem>300000] rusage[mem=300000]"
+#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/05.src_topiary_iqtree/00.log/src_topiary_iqtree_mf_090324_%J.out
+#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/05.src_topiary_iqtree/00.log/src_topiary_iqtree_mf_090324_%J.err
+#BSUB -q hugemem
+
+conda activate /software/hgi/envs/conda/team354/xl7/asr
+
+iqtree2 -s /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/seed_to_ali/08_alignment_cleaned_trimmed.fasta -m MF -cmax 8 -nt AUTO -pre /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/05.src_topiary_iqtree/01.out_mf/src_topiary_mf/src_topiary_mf
+
