82fdb6f04021f46d18d95344d87d0505200c7994
Xiaotian Liao <xl7@sanger.ac.uk>
2024-09-09 14:41:36 +0100

    housekeeping

--- 00.inputs/02.src_topiary/topiary_ali2anc.sh
+++ 00.inputs/02.src_topiary/topiary_ali2anc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #BSUB -G team354
 #BSUB -n 10  # Requesting 10 threads (cores)
-#BSUB -M 100000  # Memory requirement (adjust as needed)
-#BSUB -R "select[mem>100000] rusage[mem=100000]" 
+#BSUB -M 80000  # Memory requirement (adjust as needed)
+#BSUB -R "select[mem>80000] rusage[mem=80000]" 
 #BSUB -R "affinity[core(10)]"
-#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090624_%J.out
-#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090624_%J.err
+#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090724_yesterday_%J.out
+#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090724_yesterday_%J.err
 #BSUB -q yesterday
 
 #topiary-fasta-into-dataframe 05_clean-aligned-dataframe.csv 10_aligned_cleaned_trimed_sh3_sh2_kd.fasta final_dataframe_10.csv
@@ -14,4 +14,5 @@ conda activate  /software/hgi/envs/conda/team354/xl7/topiary
 
 cd /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary
 
-topiary-alignment-to-ancestors /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/seed_to_ali/final_dataframe_10.csv --out_dir /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali_to_anc --num_threads 10
+topiary-alignment-to-ancestors /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/seed_to_ali/final_dataframe_10_removerow258.csv --out_dir /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali_to_anc_yesterday --num_threads 10
+
---
+++ 00.inputs/02.src_topiary/topiary_ali2anc_long_09.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#BSUB -G team354
+#BSUB -n 10  # Requesting 10 threads (cores)
+#BSUB -M 80000  # Memory requirement (adjust as needed)
+#BSUB -R "select[mem>80000] rusage[mem=80000]" 
+#BSUB -R "affinity[core(10)]"
+#BSUB -o /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090724_long_09_%J.out
+#BSUB -e /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali2anc_src_090724_long_09_%J.err
+#BSUB -q long
+
+#topiary-fasta-into-dataframe 05_clean-aligned-dataframe.csv 09_alignment_cleaned_disordered_sh3_sh2_kd.fasta final_dataframe_09.csv
+
+conda activate  /software/hgi/envs/conda/team354/xl7/topiary
+
+cd /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary
+
+topiary-alignment-to-ancestors /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/seed_to_ali/final_dataframe_09.csv --out_dir /lustre/scratch126/gengen/projects/alpha-allostery-global/git-phylo-dms/00.inputs/02.src_topiary/ali_to_anc_09 --num_threads 10
+
+
